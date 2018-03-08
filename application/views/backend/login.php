6e73322b85b5edb8365916ffac19f7bcaf6db8b3
chanpisethchea <chpiseth9@gmail.com>
2018-03-08 17:22:22 -0500

    add login-form

--- application/views/backend/login.php
+++ application/views/backend/login.php
@@ -73,6 +73,61 @@ var baseurl = '<?php echo base_url();?>';
 		<div></div>
 	</div>
 	
+	<div class="login-form">
+		
+		<div class="login-content">
+			
+			<div class="form-login-error">
+				<h3>Invalid login</h3>
+				<p>Please enter correct email and password!</p>
+			</div>
+			
+			<form method="post" role="form" id="form_login">
+				
+				<div class="form-group">
+					
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="entypo-user"></i>
+						</div>
+						
+						<input type="text" class="form-control" name="email" id="email" placeholder="Email" autocomplete="off" data-mask="email" />
+					</div>
+					
+				</div>
+				
+				<div class="form-group">
+					
+					<div class="input-group">
+						<div class="input-group-addon">
+							<i class="entypo-key"></i>
+						</div>
+						
+						<input type="password" class="form-control" name="password" id="password" placeholder="Password" autocomplete="off" />
+					</div>
+				
+				</div>
+				
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block btn-login">
+						<i class="entypo-login"></i>
+						Login
+					</button>
+				</div>
+				
+						
+			</form>
+			
+			
+			<div class="login-bottom-links">
+				<a href="<?php echo base_url();?>index.php?login/forgot_password" class="link">
+					<?php echo get_phrase('forgot_your_password');?> ?
+				</a>
+			</div>
+			
+		</div>
+		
+	</div>
 	
 </div>
 
