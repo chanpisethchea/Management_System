9875076bbaa453144a460ab59184a01d3fdb074c
chanpisethchea <chpiseth9@gmail.com>
2018-03-08 21:40:49 -0500

    delete some text

--- application/views/backend/footer.php
+++ application/views/backend/footer.php
@@ -1,4 +1,4 @@
 <!-- Footer -->
 <footer class="main">
-	&copy; 2016 optimumlinkup.com.ng <strong> School Manager</strong>. 
-    Developed by Optimum Linkup </footer>
+	<strong> Management System</strong>. 
+    Developed by Seth </footer>
